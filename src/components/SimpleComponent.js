7a83ee90b46f7b4c72962905572a55f9c1208bfd
Megan Horsfield <meganthadev@gmail.com>
2020-08-25 06:38:35 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,23 @@
-// Code SimpleComponent Here
+import React from 'react'
+
+class SimpleComponent extends Component {
+  
+  constructor() {
+    super()
+    
+    this.state = {
+      mood: "happy"
+    }
+  }
+  
+  render() {
+    return(
+      <div>
+        
+      </div>
+      )
+  }
+  
+  
+  
+}
